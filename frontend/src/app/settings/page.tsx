a2353df976945a803c5e252ad8a06bd146ba5562
Pal Szentgyorgyi <pszentg@gmail.com>
2024-06-30 23:46:08 +0200

    fixed url ending

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -59,7 +59,7 @@ export default function SettingsPage() {
       await mutate(
         async () => {
           const response = await wretch(
-            `${process.env.NEXT_PUBLIC_BASE_URL}/auth/users/me`
+            `${process.env.NEXT_PUBLIC_BASE_URL}/auth/users/me/`
           )
             .auth(`Bearer ${token}`) // Use .auth() to set the Bearer token
             .put({
